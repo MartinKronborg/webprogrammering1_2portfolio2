5bdbfbf3f0aa13bc0b1f5f7a80e3efa9ff88d89f
Emil Rune <emilrune@gmail.com>
2025-12-03 14:29:24 +0100

    AccountController done

---
+++ WebProg_Portfolio2/Controllers/AccountController.cs
@@ -0,0 +1,112 @@
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+using WebProg_Portfolio2.Models;
+using System.Security.Cryptography;
+
+namespace WebProg_Portfolio2.Controllers;
+
+public class AccountController : Controller
+
+{
+    private readonly AppDbContext _database;
+
+    public AccountController(AppDbContext database)
+    {
+        _database = database;
+    }
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(string username, string email, string password)
+    {
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(email) ||
+            string.IsNullOrWhiteSpace(password))
+        {
+            ModelState.AddModelError("", "Alle felter er ikke udfyldt endnu");
+            return View();
+        }
+
+        //EMAIL check regex
+        var emailRegex = new Regex(@"/^[a-zA-Z._-]+@[a-zA-Z._-]+\.[a-z]{2,10}$/");
+        if (!emailRegex.IsMatch(email))
+        {
+            ModelState.AddModelError("", "E-mail adresse ej valid");
+            return View();
+        }
+
+        var hash = Convert.ToBase64String(SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(password)));
+
+        var user = new UsersModel
+        {
+            Username = username, Email = email, HashedPassword = password
+        };
+
+        _database.Users.Add(user);
+        await _database.SaveChangesAsync();
+
+        HttpContext.Session.SetInt32("UserId", user.Id);
+        return RedirectToAction("Gallery", "Image");
+
+        /*
+        //PASSWORD check regex
+        if (!Regex.IsMatch(password, "[a-z]"))
+        {
+            ModelState.AddModelError("", "You must include a lower-case letter");
+            return View();
+        }
+
+        if (!Regex.IsMatch(password, "[A-Z]"))
+        {
+            ModelState.AddModelError("", "You must include an upper-case letter");
+            return View();
+        }
+
+        if (!Regex.IsMatch(password, "[0-9]"))
+        {
+            ModelState.AddModelError("", "You must include a number");
+            return View();
+        }
+
+        if (!Regex.IsMatch(password, "[:;!\"#?¤%&/()=*,.^><_-]"))
+        {
+            ModelState.AddModelError("", "You must include a special character");
+            return View();
+        }
+
+        if (password.Length < 8)
+        {
+            ModelState.AddModelError("", "Your password must be at least 8 characters");
+            return View();
+        }
+
+        return View(); */
+    }
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Login(string emailOrUsername, string password)
+    {
+        var hash = Convert.ToBase64String(SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(password)));
+        var user = _database.Users.FirstOrDefault(u =>
+            (u.Email == emailOrUsername || u.Username == emailOrUsername) && u.HashedPassword == hash);
+        if (user == null)
+        {
+            ModelState.AddModelError("", "Forkert login");
+            return View();
+        }
+        HttpContext.Session.Remove("UserId");
+        return RedirectToAction("Login");
+    }
+}
