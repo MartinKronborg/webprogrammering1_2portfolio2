63b3f6dbb5d2a0b9a26a795e66de70850533b4bf
Emil Rune <emilrune@gmail.com>
2025-12-09 17:33:44 +0100

    Ready to release and upload

--- WebProg_Portfolio2/obj/Debug/net9.0/WebProg_Portfolio2.AssemblyInfo.cs
+++ WebProg_Portfolio2/obj/Debug/net9.0/WebProg_Portfolio2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProg_Portfolio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8a614539914792727d9feb7bcb562e7925eb3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+212707a055fc6f353c5d9479dfccf1db90ce4c8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProg_Portfolio2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProg_Portfolio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
